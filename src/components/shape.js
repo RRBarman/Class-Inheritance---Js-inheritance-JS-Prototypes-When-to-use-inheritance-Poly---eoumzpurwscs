2b47abe836134bb6deeb49f7f833f78427dfdac1
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-01-21 21:22:00 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+    constructor(color) {
+        this.color = "blue";
+    }
+
+    drawShape() {
+        console.log("Drawing a shape");
+    }
+
+    calculateArea() {
+        console.log("Calculating area of shape");
+    }
+}
+
+module.exports = Shape;
